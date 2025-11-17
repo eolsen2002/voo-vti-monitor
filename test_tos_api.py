9593723ab5cfd02c728e8f62cb17f4d54a2793ff
Erik O <eolsen@blacksburg.gov>
2025-11-16 19:27:59 -0500

    Update test_tos_api.py

--- test_tos_api.py
+++ test_tos_api.py
@@ -1,1 +1,12 @@
 # test_tos_api.py
+from voo_vti_fetcher import get_voo_vti_quotes
+
+print("Testing Schwab API…")
+
+result = get_voo_vti_quotes()
+
+if result:
+    print("API result:")
+    print(result)
+else:
+    print("No data returned.")
